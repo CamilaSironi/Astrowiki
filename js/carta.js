e7af3219620287a6de9bdf2f46fae2728d38f9fa
Camila Sironi <sironicamila98@gmail.com>
2023-11-12 21:12:47 -0300

    Add files via upload

--- js/carta.js
+++ js/carta.js
@@ -1,132 +1,166 @@
 'use strict'
 
-document.addEventListener("DOMContentLoaded", iniciarPag);
+    const url = "https://64af121ec85640541d4e1e17.mockapi.io/usuarios/";
+    let captcha;
 
-function iniciarPag(){
+    let btnmenu = document.getElementById("btnMenu");
+    btnmenu.addEventListener("click", () => { // desplega el menu en version mobile
+        document.getElementById("menu").classList.toggle("navigation"); 
+    });
 
-let btnmenu = document.getElementById("btnMenu");
-btnmenu.addEventListener("click", function(){
-    desplegar()});
-
-function desplegar(){
-    document.getElementById("menu").classList.toggle("navigation");
-}
-
-let arreglo = [
-    {
-        "nombre": "Juan",
-        "sexo": "Masculino",
-        "email": "juan@gmail.com",
-        "fecha": "19/10/1998",
-        "hora": "12:35",
-        "lugar": "Madariaga",
-        "familiar1": "primo",
-        "familiar2": "cuñada",
-        "familiar3": "madre"
-    }
-]
-
-mostrarTabla(arreglo);
-
-let captcha;
-
-generarCaptcha(captcha);
-
-let btn1 = document.getElementById("generador");
-btn1.addEventListener("click", function(){
-    captcha = generarCaptcha()});
-
-function generarCaptcha(){
-    captcha ="";
-    const posibles = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-    document.getElementById("ingresa").value="";
-    document.getElementById("valida").innerHTML="";
-    for (let i = 0; i <= 5; i++) {
-        captcha = captcha+posibles[Math.floor(Math.random()*(posibles.length))];
-    }
-    document.getElementById("Captchagenerado").innerHTML = captcha;
-    return captcha;
-}
-
-let btn2 = document.getElementById("validador");
-btn2.addEventListener("click", function(){
-    validarCaptcha()});
-
-function validarCaptcha(){
-    let ingresa = document.getElementById("ingresa").value;
-    console.log("ingreso: "+ingresa);
-    console.log("captcha: "+captcha);
-    if (ingresa==captcha){
-        document.getElementById("valida").innerHTML = "El captcha ingresado es válido. Puede enviar el formulario.";
-        form.addEventListener("submit", function(event){
-            event.preventDefault()
-          });
-    }
-    else {
-        document.getElementById("valida").innerHTML = "El captcha ingresado es inválido. Inténtelo de nuevo.";
-    }
-}
-
-let miembro = {};
-let nombre, sexo, email, fecha, hora, lugar;
-let form = document.getElementById("form");
-form.addEventListener("submit", agregar);
-
-function agregar() {
-    let formData = new FormData(form);
-    nombre = formData.get('nombre');
-    sexo = formData.get("sexo")
-    email = formData.get("email");
-    fecha = formData.get("fecha");
-    hora = formData.get("hora");
-    lugar = formData.get("lugar");
-
-    miembro = {
-        "nombre": nombre,
-        "sexo": sexo,
-        "email": email,
-        "fecha": fecha,
-        "hora": hora,
-        "lugar": lugar,
-        "familiar1": " ",
-        "familiar2": " ",
-        "familiar3": " "
-    }
-
-    arreglo.push(miembro);
     mostrarTabla();
-    generarCaptcha(captcha);
-}
+    generarCaptcha();
 
-let fam1, fam2, fam3;
+    let btn1 = document.getElementById("generador");
+    btn1.addEventListener("click", function(){
+        captcha = generarCaptcha()});
 
-let botonAdd = document.getElementById("btnAddFamiliares");
-botonAdd.addEventListener("click", function(){
-    agregarFamiliares()});
-
-function agregarFamiliares(){
-  miembro.familiar1 = "padre";
-  miembro.familiar2 = "tio";
-  miembro.familiar3 = "hermana";
-  mostrarTabla();
-}  
-
-let btn3 = document.getElementById("borrar");
-btn3.addEventListener("click", function(){
-    borrarTodo()});
-
-function borrarTodo(){
-    arreglo = [];
-    mostrarTabla();
-    console.table(arreglo);
-}
-
-function mostrarTabla() {
-    let tabla = document.getElementById("tablaCarta");
-    tabla.innerHTML = "";
-    tabla.innerHTML = "<thead><tr><td> Nombre </td><td> Sexo </td><td> E-mail </td><td> Fecha de nacimiento </td><td> Hora de nac </td><td> Lugar de nacimiento </td><td> Familiar 1 </td><td> Familiar 2 </td><td> Familiar 3 </td></tr></thead>"
-    for(let i = 0; i<arreglo.length; i++){
-      tabla.innerHTML = tabla.innerHTML + "<tr><td>" + arreglo[i].nombre + "</td><td>" + arreglo[i].sexo + "</td><td>" + arreglo[i].email + "</td><td>" + arreglo[i].fecha + "</td><td>" + arreglo[i].hora + "</td><td>" + arreglo[i].lugar + "</td><td>" + arreglo[i].familiar1+ "</td><td>" + arreglo[i].familiar2+ "</td><td>" + arreglo[i].familiar3 + "</td></tr>";
+    function generarCaptcha(){
+        captcha ="";
+        const posibles = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+        document.getElementById("ingresa").value="";
+        document.getElementById("valida").innerHTML="";
+        for (let i = 0; i <= 5; i++) {
+            captcha = captcha+posibles[Math.floor(Math.random()*(posibles.length))];
+        }
+        document.getElementById("Captchagenerado").innerHTML = captcha;
+        return captcha;
     }
-   }
-}
+
+    let btn2 = document.getElementById("validador");
+    btn2.addEventListener("click", () => { //fc que valida el captcha
+        let ingresa = document.getElementById("ingresa").value;
+        console.log("ingreso: "+ingresa);
+        console.log("captcha: "+captcha);
+        if (ingresa==captcha){
+            document.getElementById("valida").innerHTML = "El captcha ingresado es válido. Puede enviar el formulario.";
+            form.addEventListener("submit", (event) => {
+                event.preventDefault();
+            });
+        }
+        else {
+            document.getElementById("valida").innerHTML = "El captcha ingresado es inválido. Inténtelo de nuevo.";
+        }
+    });
+
+    let form = document.getElementById("form");
+    form.addEventListener("submit", subirData);
+
+    async function subirData(){
+        try {
+        let formData = new FormData(form); 
+        let miembro = Object.fromEntries(formData); 
+        let res = await fetch(url, { 
+                    method: 'POST',
+                    body: JSON.stringify(miembro), 
+                    headers: {'Content-Type': 'application/json'}
+                    })
+        console.log(res.status);
+        mostrarTabla();
+        generarCaptcha();
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+    async function mostrarTabla() {
+        let tabla = document.getElementById("tablaCarta");
+        tabla.innerHTML = "";
+        let res = await fetch(url); 
+        let arr = await res.json();
+        if (arr.length === 0){
+            let defaultJson = {
+                "nombre": "Juan Ramírez",
+                "sexo": "Masculino",
+                "email": "juanramirez@gmail.com",
+                "fecha": "12/05/1997",
+                "hora": "12:05",
+                "lugar": "Necochea",
+                "familiar1": "Primo",
+                "familiar2": "Hermano",
+                "familiar3": "Madre"
+            }
+            let res = await fetch(url, { 
+                method: 'POST',
+                body: JSON.stringify(defaultJson), 
+                headers: {'Content-Type': 'application/json'}
+                })
+            console.log(res.status);
+        }
+        let res1 = await fetch(url); 
+        arr = await res1.json();
+        tabla.innerHTML = "<thead><tr><td> Nombre </td><td> Sexo </td><td> E-mail </td><td> Fecha de nacimiento </td><td> Hora de nac </td><td> Lugar de nacimiento </td><td> Familiar 1 </td><td> Familiar 2 </td><td> Familiar 3 </td></tr></thead>"
+        for(let i = 0; i<arr.length; i++){ 
+            tabla.innerHTML = tabla.innerHTML + "<tr id="+arr[i].id+"><td>" + arr[i].nombre + "</td><td>" + arr[i].sexo + "</td><td>" + arr[i].email + "</td><td>" + arr[i].fecha + "</td><td>" + arr[i].hora + "</td><td>" + arr[i].lugar + "</td><td>" + arr[i].familiar1+ "</td><td>" + arr[i].familiar2+ "</td><td>" + arr[i].familiar3 + "</td><div class='renglonbtns'><button class='btn' type='button' onClick={borrar("+arr[i].id+")}>Borrar</button><button class='btn' type='button' onClick={editar("+arr[i].id+")}>Editar</button><button class='btn' type='button' onClick={agregaFam("+arr[i].id+")}>Agrega 3 familiares al azar</button></div></tr>";
+        }
+    }
+
+    async function borrar(id){
+        try{
+            let res = await fetch(url+id,{
+                method: "DELETE"
+            })
+            console.log(res.status);
+            mostrarTabla();
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+    function editar(id){
+        const linea = document.getElementById(id);
+        const tds = linea.querySelectorAll("td");
+        tds.forEach(td => {
+            td.setAttribute('contenteditable', true);
+        })
+        if(document.getElementById('btnGuardar'+id) == null){
+            let btnGuardar = document.createElement('div');
+            btnGuardar.innerHTML = "<button class='btn' id='btnGuardar"+id+"' type='button' onClick={guardar("+id+")}>Guardar</button>";
+            linea.appendChild(btnGuardar);
+        }
+    }
+        
+    async function guardar(id){
+        const nuevo = [];
+        const linea = document.getElementById(id);
+        const tds = linea.querySelectorAll("td");
+        tds.forEach(td => {
+                nuevo.push(td.innerHTML);
+        })
+        let newJson = {
+            "nombre": nuevo[0],
+            "sexo": nuevo[1],
+            "email": nuevo[2],
+            "fecha": nuevo[3],
+            "hora": nuevo[4],
+            "lugar": nuevo[5],
+            "familiar1": nuevo[6],
+            "familiar2": nuevo[7],
+            "familiar3": nuevo[8]
+        }
+        let res = await fetch(url+id, {
+            method: "PUT",
+            body: JSON.stringify(newJson),
+            headers: {'Content-Type': 'application/json'}
+        })
+        console.log(res.status);
+        mostrarTabla();
+        }
+
+    async function agregaFam(id){
+        let payload = {
+            familiar1: "padre",
+            familiar2: "tio",
+            familiar3: "hermana"
+        }
+        let res = await fetch(url+id, {
+            method: "PUT",
+            body: JSON.stringify(payload),
+            headers: {'Content-Type': 'application/json'}
+        })
+        console.log(res.status);
+        mostrarTabla();
+    }
+
